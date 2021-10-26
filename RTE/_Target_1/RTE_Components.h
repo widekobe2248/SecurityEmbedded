1211287f19650fe1b5138836690eee4502472c92
Kobe Johnson <kobejohnson7771@gmail.com>
2021-10-26 14:33:42 -0400

    Base Files

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'EmbeddedLab' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+/* Keil.Standalone::Device:Startup:1.0.0 */
+#define RTE_DEVICE_STARTUP_STM32L0XX    /* Device Startup for STM32L0 */
+
+
+#endif /* RTE_COMPONENTS_H */
