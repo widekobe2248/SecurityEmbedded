e9718b7379267f1cc3cda0903877c44c8486cf49
Kobe Johnson <kobejohnson7771@gmail.com>
2021-10-26 14:49:34 -0400

    Added Main.c structure

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 //Main Files
+
+//Base Include 
+#include "stm32l053xx.h"
+#include <stdint.h>
+
+int main(void)
+{
+	
+	//Object Declaration
+	
+	
+	//PIR Warm-Up (x Amount of Time)
+	
+	
+	//Schedule Loop For Tasks
+	while(1)
+	{
+		break;
+	}
+}
